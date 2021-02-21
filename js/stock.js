de3566e253d60e2b5727757dacef5ea541097399
chenpeng57 <chenpeng57@meituan.com>
2021-02-21 11:05:49 +0800

    里程碑

--- js/stock.js
+++ js/stock.js
@@ -21,7 +21,7 @@ function getQueryString(name, de) {
     }
     return de;
 }
-Date.prototype.addDays = function(number) {
+Date.prototype.addDays = function (number) {
     var adjustDate = new Date(this.getTime() + 24 * 60 * 60 * 1000 * 30 * number)
     return adjustDate;
 }
@@ -40,7 +40,7 @@ function dateFormat(fmt, date) {
         "H+": date.getHours().toString(), // 时
         "M+": date.getMinutes().toString(), // 分
         "S+": date.getSeconds().toString() // 秒
-            // 有其他格式化字符需求可以继续添加，必须转化成字符串
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
     };
     for (let k in opt) {
         ret = new RegExp("(" + k + ")").exec(fmt);
@@ -51,13 +51,42 @@ function dateFormat(fmt, date) {
     return fmt;
 }
 
-var clone = function(obj) {
+var clone = function (obj) {
     return JSON.parse(JSON.stringify(obj));
-}
+};
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.hash = factory();
+    }
+}(this, function () {
+    /**
+        A string hashing function based on Daniel J. Bernstein's popular 'times 33' hash algorithm.
+        @param {string} text - String to hash
+        @return {number} Resulting number.
+    */
+    function hash(text) {
+        'use strict';
+
+        var hash = 5381,
+            index = text.length;
+
+        while (index) {
+            hash = (hash * 33) ^ text.charCodeAt(--index);
+        }
+
+        return hash >>> 0;
+    }
+    return hash;
+}));
 
 function jsonp(options) {
     return new Promise((resolve, reject) => {
-        let callbackID = `jsonp_${Date.now()}_${Math.ceil(Math.random() * 100000)}`, //随机生成callbackID
+        let callbackID = "jsonp_" + hash(options.url), //随机生成callbackID
             container = document.getElementsByTagName('head')[0],
             scriptNode = document.createElement("script"),
             data = options.data || {},
@@ -75,7 +104,7 @@ function jsonp(options) {
         scriptNode.id = options.data.code; //设置script节点id以便后面删除
         scriptNode.src = url;
         if (!window[callbackID]) {
-            window[callbackID] = function(response) {
+            window[callbackID] = function (response) {
                 //定义全局函数，注意函数名是callbackID是跟上面定义的参数data["callback"]=callbackID是一致的
                 // 服务端接口是根据客户端传的callback而返回callbackID({"code":0,"error":"操作成功","data":{}})
                 var id = response.symbol;
@@ -128,7 +157,7 @@ function jsonp(options) {
     })
 }
 
-var variableName = function(code) {
+var variableName = function (code) {
     // hq_str_sh600519
     return "hq_str_" + (code >= 600000 ? 'sh' : 'sz') + code;
 }
@@ -136,7 +165,7 @@ var variableName = function(code) {
 function loadScript(url, callback) {
     let script = document.createElement('script');
     if (script.readyState) { // IE
-        script.onreadystatechange = function() {
+        script.onreadystatechange = function () {
             if (script.readyState === 'loaded' || script.readyState === 'complete') {
                 script.onreadystatechange = null;
                 // console.log(globalThis[variableName(stockCodeArr[0])]);
@@ -144,7 +173,7 @@ function loadScript(url, callback) {
             }
         }
     } else { // 其他浏览器
-        script.onload = function() {
+        script.onload = function () {
             stockCodeArr.forEach(e => {
                 var prefix = e >= 600000 ? 'SSE' : 'SZSE';
                 var code = prefix + e;
@@ -160,7 +189,7 @@ function loadScript(url, callback) {
         container.appendChild(script);
 }
 
-var loadStockNames = function(arr) {
+var loadStockNames = function (arr) {
     var stockCodeArr = [];
     for (let i = 0; i < arr.length; i++) {
         const e = arr[i];
@@ -173,7 +202,7 @@ var loadStockNames = function(arr) {
 
 
 
-var getStockKlineList = function(arr, dateUnit) {
+var getStockKlineList = function (arr, dateUnit) {
     dateUnit = dateUnit ? dateUnit : 'month'
     var r = [];
     arr.forEach(code => {
@@ -197,7 +226,7 @@ var getStockKlineList = function(arr, dateUnit) {
     return r;
 }
 
-var getStockInfo = function(arr) {
+var getStockInfo = function (arr) {
     var r = [];
     arr.forEach(code => {
         var market = '';
@@ -220,7 +249,7 @@ var getStockInfo = function(arr) {
     return r;
 }
 
-var getStockNameToStockArrMap = function(raw) {
+var getStockNameToStockArrMap = function (raw) {
     var nameToStockArrMap = new Map();
     for (let i = 1; i < raw.length; i++) {
         const e = raw[i];
